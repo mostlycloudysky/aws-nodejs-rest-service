d5f8a7ed58a8cdfd9b06a2c81564e16a813a2ef3
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-10-28 15:07:05 -0400

    final update

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,8 +2,16 @@ const axios = require('axios')
 const aws = require('aws-sdk')
 let response;
 const secretManagerClient = new aws.SecretsManager();
+const sesClient = new aws.SES()
 
 exports.lambdaHandler = async (event, context) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+    }
+     
+    const zipcode = event.queryStringParameters.zipcode
+    const country = event.queryStringParameters.country
+
     try {
 
         const result = await secretManagerClient.getSecretValue({
@@ -12,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     
         const openweatherAPI = JSON.parse(result.SecretString)
         // @ts-ignore
-        const ret = await axios(`https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${openweatherAPI.openweatherapikey}`);
+        const ret = await axios(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&appid=${openweatherAPI.openweatherapikey}`);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
@@ -24,6 +32,28 @@ exports.lambdaHandler = async (event, context) => {
         console.log(err);
         return err;
     }
+
+    // Send email using AWS SES
+    const emailParams = {
+        Source: 'ysandeepkumar88@gmail.com', 
+        Destination: {
+          ToAddresses: ['ysandeepkumar88@gmail.com'], 
+        },
+        Message: {
+          Body: {
+            Text: {
+              Charset: 'UTF-8',
+              Data: `${JSON.stringify(response)}`,
+            },
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: 'AWS SES weather data - email',
+          },
+        },
+    };
+
+    await sesClient.sendEmail(emailParams).promise()
 
     return response
 };
