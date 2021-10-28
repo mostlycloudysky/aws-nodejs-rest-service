b7dbdcad98070a0383edaba51a9ddae2b26ae355
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-10-28 11:32:50 -0400

    more updates

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,27 +1,23 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const axios = require('axios')
+const aws = require('aws-sdk')
 let response;
+const secretManagerClient = new aws.SecretsManager();
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
+
+        const result = await secretManagerClient.getSecretValue({
+            SecretId: 'openweatherapi'
+        }).promise()
+    
+        const openweatherAPI = JSON.parse(result.SecretString)
+        // @ts-ignore
+        const ret = await axios(`https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${openweatherAPI.openweatherapikey}`);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
+                message: 'Weather data from openweathermap',
+                data: ret.data
             })
         }
     } catch (err) {
